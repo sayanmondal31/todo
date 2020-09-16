fb41bbadb89a1985823bf81fd4db2fa559a9825c
sayanmondal31 <sayanm816@gmail.com>
2020-09-16 17:09:45 +0530

    todo: => complete page add

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:todo/models/task.dart';
 import 'dart:collection';
@@ -5,13 +6,14 @@ import 'dart:collection';
 class TaskData extends ChangeNotifier {
   List<Task> _tasks = [];
 
-  UnmodifiableListView<Task> get tasks {
-    return UnmodifiableListView(_tasks);
-  }
+  List<Task> _completedTask = [];
 
-  int get totalTask {
-    return _tasks.length;
-  }
+
+  UnmodifiableListView<Task> get tasks => UnmodifiableListView(_tasks);
+  UnmodifiableListView<Task> get completdTasks => UnmodifiableListView(_completedTask);
+  
+  int get totalTask => _tasks.length;
+  int get completeTotalTask => _completedTask.length;
 
   void addTask(String updatedDatalist) {
     final task = Task(name: updatedDatalist);
@@ -19,12 +21,24 @@ class TaskData extends ChangeNotifier {
     notifyListeners();
   }
 
+  
+
   void checkBoxtick(Task task) {
     task.toggledone();
+    task.isdone?_completedTask.add(task):_completedTask.remove(task);
+    print(_completedTask);
     notifyListeners();
   }
 
   void deleteTask(Task task) {
     _tasks.remove(task);
+    _completedTask.remove(task);
+    notifyListeners();
+  }
+
+  void deleteAll() {
+    _tasks.clear();
+    _completedTask.clear();
+    notifyListeners();
   }
 }
